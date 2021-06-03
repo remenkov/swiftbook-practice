74333031b336e39a8c03add4713d06da95bb8b31
Roman Remenkov <remenkov@gmail.com>
2021-06-03 16:58:13 +1000

    Add Task 4. Iterate dictionary and add value to variable

--- SwiftbookWebinars2.3/SwiftbookWebinars2.3/main.swift
+++ SwiftbookWebinars2.3/SwiftbookWebinars2.3/main.swift
@@ -100,3 +100,78 @@ for (key, value) in someSchedules {
     print(key, value)
 }
 printDelimiter()
+
+
+/*
+ Создайте переменную total типа Double, равную 0.
+ Переберите все значения словаря, и добавьте значение каждого целого и вещественного числа к
+ значению вашей переменной.
+ Для каждой строки добавьте 1.
+ Для каждого булева значения, добавьте 2, в случае, если значение равно true, либо вычтите 3,
+ если оно false.  Напечатайте значение total.
+ */
+
+var total = 0.0
+
+for value in someSchedules.values {
+    switch value {
+        case let intValue as Int:
+            total += Double(intValue)
+        case let doubleValue as Double:
+            total += doubleValue
+        case is String:
+            total += 1
+        case let boolValue as Bool:
+            total += boolValue ? 1 : -3
+        default:
+            Void()
+    }
+}
+
+print(total)
+printDelimiter()
+
+total = 0.0
+
+for value in someSchedules.values {
+    if let value = value as? Int {
+        total += Double(value)
+
+    } else if let value = value as? Double {
+        total += value
+
+    } else if value is String {
+        total += 1
+
+    } else if let value = value as? Bool {
+        total += value ? 2 : -3
+    }
+}
+
+printDelimiter()
+print("Total: \(total)")
+printDelimiter()
+
+/*
+ Обнулите переменную total и снова пройдите по всей коллекции, прибавляя к ней все целые и
+ вещественные числа.  Для каждой строки, встретившейся на пути, попробуйте сконвертировать её в
+ число, и добавьте это значение к общему.  Игнорируйте булевы значения.  Распечатайте итог.
+*/
+
+total = 0.0
+
+for value in someSchedules.values {
+    if let value = value as? Int {
+        total += Double(value)
+
+    } else if let value = value as? Double {
+        total += value
+
+    } else if let value = value as? String, let value = Double(value)  {
+        total += value
+    }
+}
+
+printDelimiter()
+print("Total: \(total)")
+printDelimiter()
